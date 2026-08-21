b3648b10cd1da2509c3f09b6aaeffd6044c501b3
shazzadmahe11-commits <shazzadmahe11@gmail.com>
2026-08-21 00:47:09 -0300

    Update ThemeToggle.tsx

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -15,6 +15,8 @@ export default function ThemeToggle() {
     setTheme(next);
     document.documentElement.setAttribute('data-theme', next);
     localStorage.setItem('theme', next);
+    const meta = document.querySelector('meta[name="theme-color"]');
+    if (meta) meta.setAttribute('content', next === 'light' ? '#f7f8fb' : '#12141a');
   };
 
   return (
